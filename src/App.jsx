fd94456644b881e6ef88d8a3159557e8e5e3a2e9
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2024-11-23 00:54:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <h1 className='text-3xl text-red-500 text-bold'>Welcome to awwwards</h1>
+        </main>
+    )
+}
+export default App
