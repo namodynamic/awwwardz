c40adbc8ad45e4539e0c10314d14b87c8e8fee8b
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2024-11-23 23:23:41 +0100

    Implement Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
-    return (
-        <main>
-            <h1 className='text-3xl text-red-500 text-bold'>Welcome to awwwards</h1>
-        </main>
-    )
-}
-export default App
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="bg-blue-500 min-h-screen z-0 " />
+    </main>
+  );
+};
+export default App;
